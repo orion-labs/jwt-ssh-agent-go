5dea4fedf1c14c4475808ea5f0db6dfc980694d1
Nik Ogura <nik@orionlabs.io>
2019-12-31 12:19:26 -0800

    initial working version

---
+++ pkg/agentjwt/server.go
@@ -0,0 +1,55 @@
+package agentjwt
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
+)
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+type TestServer struct {
+	Address    string
+	Port       int
+	ServerRoot string
+	PubkeyFunc func(username string) (pubkey string, err error)
+}
+
+// Run runs the test repository server.
+func (d *TestServer) RunRepoServer() (err error) {
+
+	log.Printf("Running dbt artifact server on %s port %d.  Serving tree at: %s", d.Address, d.Port, d.ServerRoot)
+
+	fullAddress := fmt.Sprintf("%s:%s", d.Address, strconv.Itoa(d.Port))
+
+	r := mux.NewRouter()
+
+	r.Handle("/", http.HandlerFunc(d.RootHandler))
+
+	err = http.ListenAndServe(fullAddress, r)
+
+	return err
+}
+
+func (d *TestServer) RootHandler(w http.ResponseWriter, r *http.Request) {
+	tokenString := r.Header.Get("Token")
+
+	// Parse the token, which includes setting up it's internals so it can be verified.
+	subject, token, err := ParsePubkeySignedToken(tokenString, d.PubkeyFunc)
+	if err != nil {
+		log.Errorf("Error: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !token.Valid {
+		log.Info("Auth Failed")
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	log.Infof("Subject %s successfuly authenticated", subject)
+}
